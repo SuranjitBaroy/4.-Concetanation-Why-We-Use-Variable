a7187cfe96ebe8a0acd2ab3b0bd33964b3733b87
Suranjit Baroy <62200368+SuranjitBaroy@users.noreply.github.com>
2021-06-17 12:29:34 +0600

    Add files via upload

---
+++ learn.js
@@ -0,0 +1,37 @@
+// JS Concetanation + Why we Use Variable
+
+
+var Fname = "Suranjit";
+
+var Lname = "Baroy";
+
+
+console.log(Fname,Lname);
+
+
+var Fname = "Suranjit";
+
+var Lname = "Baroy";
+
+
+console.log(Fname + " "+ Lname);
+
+
+var My = "My roll Number is";
+
+var roll = 10;
+
+
+console.log(My,roll);
+
+console.log(My + " " + roll);
+
+
+
+//Why we use variable simple examples
+
+var Variable = "Python";
+
+
+document.write(Variable + " is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does "+ Variable + " than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video");
+
